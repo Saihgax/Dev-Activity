25dc8a41288fa296378c624d0f095ae61b3e8047
Saihgax <pranavsaihgal911@gmail.com>
2021-07-25 20:48:12 +0530

    branches

--- commands/organize.js
+++ commands/organize.js
@@ -1,7 +1,39 @@
-function organizeFn(src){
-    console.log("organize command executed with " + src);
+let types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
 }
 
-module.exports = {
-    organizeFun: organizeFn
+let fs = require("fs");
+let path = require("path");
+// let ex = '.mkv'
+// console.log(typeof types["media"][0])
+// console.log(types.media.includes(ex.slice(1)))
+
+function isInTypes(entityExtn){
+    for(let i=0;i<types.length;i++){
+        for(let key in types){
+            if(types[key].includes(entityExtn.slice(1)){
+                return key;
+            }
+        }
+    }
 }
+
+function organizeFn(srcPath){ //We obtained the path for random folder
+    fs.mkdirSync("organized");
+    let newDest = path.join(srcPath, "organized");
+    let allEntities = fs.readdirSync(srcPath);
+
+    for(let i=0;i<allEntities.length;i++){
+        let entityName = allEntities[i];
+        let entityPath = path.join(newDest, entityName);
+        let designatedEntityFolder = isInTypes(path.extname(entityPath))
+    }
+
+}
+
+// module.exports = {
+//     organizeFun: organizeFn
+// }
