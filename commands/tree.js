cd16b3c02f3110ce08382779a8d3f90932e47d78
Saihgax <pranavsaihgal911@gmail.com>
2021-07-31 10:56:44 +0530

    updated repo

--- commands/tree.js
+++ commands/tree.js
@@ -1,5 +1,19 @@
+let fs = require("fs");
+let path = require("path");
+
 function treeFn(src){
-    console.log("tree command executed with " + src);
+    if(src==undefined){
+        src = process.cwd();
+    }
+
+    let content = fs.readdirSync(src);
+    let parentFolderName = path.basename(src);
+    let completePath = "└──" + parentFolderName;
+    // console.log(completePath);
+    for(let i=0;i<content.length;i++){
+        completePath += "\n\t" + "├──" + content[i];
+    }
+    console.log(completePath);
 }
 
 module.exports = {
